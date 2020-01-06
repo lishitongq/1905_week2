c780d383695f9e27832f7fde322abd7fc13546ae
lishitingq <2603258629@qq.com>
2020-01-06 07:42:40 +0000

    init

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    凌凡
+                    week2
                 </div>
 
                 <div class="links">
